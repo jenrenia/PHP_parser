f094a1f8eff24885ea4a472ec9b2490201bca1fb
qtip34 <z1x212qw@gmail.com>
2017-10-12 02:40:48 +0300

    done aiming on "all news" div

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
 <?php
-	
+	$html = file_get_contents('http://www.pravda.com.ua/rus/news/');
+	$pieces = explode('class="news news_all"', $html);
+	$html = explode('class="archive-navigation"', $pieces[1]);
+	echo $html[0];
 ?>
