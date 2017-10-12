1e2a53562e080b016363dc8501d04fcf76a0bcee
qtip34 <z1x212qw@gmail.com>
2017-10-12 20:51:40 +0300

    Repaired href appearing.

--- handler.php
+++ handler.php
@@ -10,19 +10,23 @@
 			$parsed_string = "<table><tr><td>Номер</td><td>Время и дата парсинга</td>" . 
 			"<td>Время новости</td><td>Заголовок</td><td>Текст новости</td><td>Метки</td></tr>";
 			if($html->innertext!=''){
-				if(!is_null($number)){
+				if($number != 0){
+					foreach ($html->find('.article') as $k => $v) {
+						$d = $v->find('a');
+						echo $d[0]->href;
+					}
 				  	foreach($html->find('.article') as $key => $a){
 				  		if($key + 1 <= $number){
 				  			//write to session for possible further saving to file
 				  			$_SESSION['key' . $key]['number'] = $key;
 				  			$_SESSION['key' . $key]['parsing_time'] = date('l jS \of F Y h:i:s A');
 				  			$_SESSION['key' . $key]['news_time'] = strval($html->find('.article__time')[$key]);
-				  			$_SESSION['key' . $key]['title'] = $a->href;
-				  			$_SESSION['key' . $key]['body'] = $a->plaintext;
+				  			$_SESSION['key' . $key]['title'] = strval($a->find('a')[0]->href);
+				  			$_SESSION['key' . $key]['body'] = $a->href;
 				  			$_SESSION['key' . $key]['sings'] = strval($html->find('.article__subtitle')[$key]);
 				  			//preparing string out for main div
 					    	$parsed_string = $parsed_string . '<tr><td>'. $key . '</td><td>' . date('l jS \of F Y h:i:s A') .
-					    	'</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'.$a->href.'">' .
+					    	'</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'. $a->find('a')[0]->href .'">' .
 					    	$a->plaintext.'</a></td><td>' . $html->find('.article__subtitle')[$key] .'</td></tr>';
 				    	}
 				  	}
@@ -38,7 +42,7 @@
 				  			$_SESSION['key' . $key]['sings'] = strval($html->find('.article__subtitle')[$key]);
 				  			//preparing string out for main div
 					    	$parsed_string = $parsed_string . '<tr><td>'. $key . '</td><td>' . date('l jS \of F Y h:i:s A') 
-					    	.'</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'.$a->href.'">'
+					    	.'</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'. $a->find('a')[0]->href .'">'
 					    	.$a->plaintext.'</a></td><td>' . $html->find('.article__subtitle')[$key] .'</td></tr>';
 				  	}
 			  	}  	
