835b6d6491b121339d1327ef37fb892721396a28
qtip34 <z1x212qw@gmail.com>
2017-10-12 10:10:08 +0300

    Shows only needed news
    LIbrary works strange with multiple classes

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 <?php
 	include 'libraries/simple_html_dom.php';
 	$html = file_get_html('http://www.pravda.com.ua/rus/news/');
+	
+	$html = $html->find('.block_news_all');//str_get_html($html->innertext);
+	$html = str_get_html($html[0]);
 	echo "<table>";
 	if($html->innertext!='' and count($html->find('a'))){
 	  	foreach($html->find('.article') as $key => $a){
-	    	echo '<tr><td>'. $key . '</td><td>' . $html->find('.article__time')[$key-27] . '</td><td><a href="'.$a->href.'">'.$a->plaintext.'</a></td></tr>';
+	    	echo '<tr><td>'. $key . '</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'.$a->href.'">'.$a->plaintext.'</a></td></tr>';
 	  	}
 	  	
 	}
