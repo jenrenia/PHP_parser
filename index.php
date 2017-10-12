b6c2a040358b500ca85c5f1f435e3c0fe11c2bee
qtip34 <z1x212qw@gmail.com>
2017-10-12 10:24:44 +0300

    All columns added

--- index.php
+++ index.php
@@ -6,11 +6,13 @@
 	$html = $html->find('.block_news_all');//str_get_html($html->innertext);
 	$html = str_get_html($html[0]);
 	echo "<table>";
+	echo "<tr><td>Номер</td><td>Время и дата парсинга</td><td>Время новости</td><td>Заголовок</td><td>Текст новости</td><td>Метки</td></tr>";
 	if($html->innertext!='' and count($html->find('a'))){
 	  	foreach($html->find('.article') as $key => $a){
-	    	echo '<tr><td>'. $key . '</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'.$a->href.'">'.$a->plaintext.'</a></td></tr>';
-	  	}
-	  	
+	    	echo '<tr><td>'. $key . '</td><td>' . date('l jS \of F Y h:i:s A') 
+	    	.'</td><td>' . $html->find('.article__time')[$key] . '</td><td><a href="'.$a->href.'">'
+	    	.$a->plaintext.'</a></td><td>' . $html->find('.article__subtitle')[$key] .'</td></tr>';
+	  	}  	
 	}
 	echo "</table>";
 ?>
